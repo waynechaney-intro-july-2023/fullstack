4f7b8986cdb1edd640a828f633ed820dcd4cc419
Wayne Chaney <waynechane@gmail.com>
2023-07-17 14:44:22 -0400

    more

--- frontend/app/src/app/componets/header.componet.ts
+++ frontend/app/src/app/componets/header.componet.ts
@@ -8,7 +8,7 @@ import { Component } from "@angular/core";
     template: `
 <header>
 <h1>Intro to Programming</h1>
-<p>Sample Full-Stack Application</p>
+<p>Sample Full-Stack Application!!</p>
 </header>
     `
 })
